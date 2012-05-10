a04a2b451a8b1aaa3db3b886ed09a3d030d7ca7e
drocta <madaco.madaco@gmail.com>
2012-05-10 16:13:03 -0700

    jslint does not like inline if

--- Pickle.js
+++ Pickle.js
@@ -16,12 +16,18 @@ function array_key_exists(key, array) {
 }
 function explode (delimiter, string, limit) {
 
-    if ( arguments.length < 2 || typeof delimiter == 'undefined' || typeof string == 'undefined' ) return null;
-	if ( delimiter === '' || delimiter === false || delimiter === null) return false;
+    if ( arguments.length < 2 || typeof delimiter == 'undefined' || typeof string == 'undefined' ) {
+        return null;
+    }
+	if ( delimiter === '' || delimiter === false || delimiter === null) {
+		return false;
+	}
 	if ( typeof delimiter == 'function' || typeof delimiter == 'object' || typeof string == 'function' || typeof string == 'object'){
 		return { 0: '' };
 	}
-	if ( delimiter === true ) delimiter = '1';
+	if ( delimiter === true ) {
+		delimiter = '1';
+	}
 	
 	// Here we go...
 	delimiter += '';
@@ -30,10 +36,14 @@ function explode (delimiter, string, limit) {
 	var s = string.split( delimiter );
 	
 
-	if ( typeof limit === 'undefined' ) return s;
+	if ( typeof limit === 'undefined' ) {
+		return s;
+	}
 	
 	// Support for limit
-	if ( limit === 0 ) limit = 1;
+	if ( limit === 0 ) {
+		limit = 1;
+	}
 	
 	// Positive limit
 	if ( limit > 0 ){
@@ -42,7 +52,9 @@ function explode (delimiter, string, limit) {
 	}
 
 	// Negative limit
-	if ( -limit >= s.length ) return [];
+	if ( -limit >= s.length ) {
+		return [];
+	}
 	
 	s.splice( s.length + limit );
 	return s;
