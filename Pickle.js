91417c63e107597beeeca55ee1b3a51582a400b4
drocta <madaco.madaco@gmail.com>
2012-05-10 15:56:42 -0700

    added explode

--- Pickle.js
+++ Pickle.js
@@ -9,6 +9,39 @@ function array_key_exists(key, array) {
 		return false;
 	}*/
 }
+function explode (delimiter, string, limit) {
+
+    if ( arguments.length < 2 || typeof delimiter == 'undefined' || typeof string == 'undefined' ) return null;
+	if ( delimiter === '' || delimiter === false || delimiter === null) return false;
+	if ( typeof delimiter == 'function' || typeof delimiter == 'object' || typeof string == 'function' || typeof string == 'object'){
+		return { 0: '' };
+	}
+	if ( delimiter === true ) delimiter = '1';
+	
+	// Here we go...
+	delimiter += '';
+	string += '';
+	
+	var s = string.split( delimiter );
+	
+
+	if ( typeof limit === 'undefined' ) return s;
+	
+	// Support for limit
+	if ( limit === 0 ) limit = 1;
+	
+	// Positive limit
+	if ( limit > 0 ){
+		if ( limit >= s.length ) return s;
+		return s.slice( 0, limit - 1 ).concat( [ s.slice( limit - 1 ).join( delimiter ) ] );
+	}
+
+	// Negative limit
+	if ( -limit >= s.length ) return [];
+	
+	s.splice( s.length + limit );
+	return s;
+}
 function ord(string) {
     //found on phpjs.org
     // http://kevin.vanzonneveld.net
